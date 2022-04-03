cc8e73443757ffa26cfbf1da17930eac3653064b
Izunyaaan <kanebluerivers@gmail.com>
2022-04-04 05:11:58 +0800

    added some stuff

---
+++ src/components/Map.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="map-wrap">
+    <a href="https://www.maptiler.com" class="watermark"
+      ><img
+        src="https://api.maptiler.com/resources/logo.svg"
+        alt="MapTiler logo"
+    /></a>
+    <div class="map" ref="mapContainer"></div>
+  </div>
+</template>
+
+<script>
+import { Map, NavigationControl, Marker, Popup } from "maplibre-gl";
+import { shallowRef, onMounted, onUnmounted, markRaw } from "vue";
+export default {
+  name: "MapW",
+  setup() {
+    const mapContainer = shallowRef(null);
+    const map = shallowRef(null);
+    onMounted(() => {
+      const test = () => {
+        return [Math.random() / 10 + 103.8, Math.random() / 10 + 1.3];
+      };
+      let aTest = test();
+      const apiKey = "Ls2nQjVJK9TVInhXJ7Oj";
+      if (apiKey == null) {
+        throw new Error(
+          "You need to configure env VUE_APP_API_KEY first, see README"
+        );
+      }
+      const initialState = {
+        lng: 103.8023514142483,
+        lat: 1.3695991684760638,
+        zoom: 11,
+      };
+      map.value = markRaw(
+        new Map({
+          container: mapContainer.value,
+          style: `https://api.maptiler.com/maps/streets/style.json?key=${apiKey}`,
+          center: [initialState.lng, initialState.lat],
+          zoom: initialState.zoom,
+        })
+      );
+      map.value.addControl(new NavigationControl(), "top-right");
+      const marker = new Marker({ color: "#0000FF" })
+        .setLngLat(aTest)
+        .setPopup(new Popup().setHTML("<h1>Fake Taxi #1</h1>"))
+        .addTo(map.value);
+      const marker2 = new Marker({ color: "#0000FF" })
+        .setLngLat([initialState.lng, initialState.lat])
+        .setPopup(new Popup().setHTML("<h1>Real Taxi #1</h1>"))
+        .addTo(map.value);
+
+      fetch("https://api.data.gov.sg/v1//transport/taxi-availability")
+        .then((res) => res.json())
+        .then((data) => {
+          console.log("===========");
+          console.log(data.features[0].geometry.coordinates[0]);
+          marker2.setLngLat([
+            data.features[0].geometry.coordinates[0][0],
+            data.features[0].geometry.coordinates[0][1],
+          ]);
+        });
+      setInterval(() => {
+        fetch("https://api.data.gov.sg/v1//transport/taxi-availability")
+          .then((res) => res.json())
+          .then((data) => {
+            console.log("===========");
+            console.log(data.features[0].geometry.coordinates[0]);
+            marker2.setLngLat([
+              data.features[0].geometry.coordinates[0][0],
+              data.features[0].geometry.coordinates[0][1],
+            ]);
+          });
+        aTest = test();
+        marker.setLngLat(aTest);
+      }, 30000);
+    });
+    onUnmounted(() => {
+      map.value?.remove();
+    });
+    return {
+      map,
+      mapContainer,
+    };
+  },
+};
+</script>
+<style scoped>
+@import "~maplibre-gl/dist/maplibre-gl.css";
+.map-wrap {
+  position: relative;
+  width: 100%;
+  height: calc(
+    100vh - 77px
+  ); /* calculate height of the screen minus the heading */
+}
+.map {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+.watermark {
+  position: absolute;
+  left: 10px;
+  bottom: 10px;
+  z-index: 999;
+}
+</style>
