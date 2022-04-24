20372767d90da84f00c2839f1ece1c3fdd3048ef
Izunyaaan <kanebluerivers@gmail.com>
2022-04-25 00:01:36 +0800

    fixed netlify redirect issues

--- src/router/index.js
+++ src/router/index.js
@@ -30,7 +30,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import('../views/GameView.vue')
-  }
+  },
+  {
+    path: "/*",
+    component: 404
+  },
 ]
 
 const router = createRouter({
